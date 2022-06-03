d618c5a82dc18f842c343f815f93da72dabdd575
Metody Aleksov <teddy57603@gmail.com>
2022-06-03 21:34:18 +0300

    finished toString override in MortgageCalculator.class

--- src/com/company/MortgageCalculator.java
+++ src/com/company/MortgageCalculator.java
@@ -9,35 +9,39 @@ public class MortgageCalculator {
 
     private int principal;
     private float annualInterest;
-    private float monthlyInterest;
     private byte years;
-    private int numberOfPayments;
 
-    private double mortgage;
+    public MortgageCalculator(){}
 
     public MortgageCalculator(int principal, float annualInterest, byte years){
         this.setPrincipal(principal);
         this.setAnnualInterest(annualInterest);
         this.setYears(years);
-        this.setMonthlyInterest();
-        this.setNumberOfPayments();
-        this.setMortgage();
     }
 
     public String getFormattedMortgage(){
-        return NumberFormat.getCurrencyInstance().format(mortgage);
+        return NumberFormat.getCurrencyInstance().format(getMortgage());
     }
 
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
 
-        sb.append("----------------");
+        sb.append("------------------");
         sb.append("\nTotal Mortgage: " + getFormattedMortgage());
+        sb.append("\nMonthly payment amount: " + NumberFormat.getCurrencyInstance().format(getMonthlyPayment()));
         sb.append("\nNumber of payments: " + getNumberOfPayments());
-        sb.append("\n----------------");
+        sb.append("\n------------------");
+
+        for (int i = getNumberOfPayments(); i >= 0; i--){
+            sb.append("\n" + NumberFormat.getCurrencyInstance().format(getPayment(i)));
+        }
 
         return sb.toString();
+    }
+
+    private double getPayment(int paymentNumber){
+        return getMonthlyPayment() * paymentNumber;
     }
 
     //Setters and getters
@@ -45,7 +49,7 @@ public class MortgageCalculator {
         return principal;
     }
 
-    private void setPrincipal(int principal) {
+    public void setPrincipal(int principal) {
         if(principal <= 0){
             throw new IllegalArgumentException("Principal cannot be less than or equal to 0!");
         }
@@ -56,47 +60,39 @@ public class MortgageCalculator {
         return annualInterest;
     }
 
-    private void setAnnualInterest(float annualInterest) {
+    public void setAnnualInterest(float annualInterest) {
         if(annualInterest <= 0){
             throw new IllegalArgumentException("Annual interest cannot be less than or equal to 0!");
         }
         this.annualInterest = annualInterest;
     }
 
-    private float getMonthlyInterest() {
-        return monthlyInterest;
-    }
-
-    private void setMonthlyInterest() {
-        this.monthlyInterest = getAnnualInterest() / MONTHS_IN_YEAR / PERCENT;
+    public float getMonthlyInterest() {
+        return getAnnualInterest() / MONTHS_IN_YEAR / PERCENT;
     }
 
     private byte getYears() {
         return years;
     }
 
-    private void setYears(byte years) {
+    public void setYears(byte years) {
         if(years <= 0 || years > 100){
             throw new IllegalArgumentException("Years must be between 1 and 100");
         }
         this.years = years;
     }
 
-    private int getNumberOfPayments() {
-        return numberOfPayments;
-    }
-
-    private void setNumberOfPayments() {
-        this.numberOfPayments = getYears() * MONTHS_IN_YEAR;
+    public int getNumberOfPayments() {
+        return getYears() * MONTHS_IN_YEAR;
     }
 
     public double getMortgage() {
-        return mortgage;
+        return principal *
+                (getMonthlyInterest() * Math.pow(1+getMonthlyInterest(),getNumberOfPayments()))
+                /(Math.pow(1+getMonthlyInterest(),getNumberOfPayments())-1);
     }
 
-    private void setMortgage() {
-        this.mortgage = principal *
-                (monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayments))
-                /(Math.pow(1+monthlyInterest,numberOfPayments)-1);;
+    public double getMonthlyPayment(){
+        return getMortgage() / getNumberOfPayments();
     }
 }
