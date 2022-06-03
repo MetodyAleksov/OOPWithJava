8f70ecddd25af4c8bd134bcb94b068376a82d685
Metody Aleksov <teddy57603@gmail.com>
2022-06-03 21:34:32 +0300

    added final logic to main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,18 +10,46 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
+        MortgageCalculator mc = new MortgageCalculator();
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print("Principal: ");
+            int principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+            try{
+                mc.setPrincipal(principal);
+                break;
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
 
-        System.out.print("Period (Years): ");
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
 
-        byte years = scanner.nextByte();
+            try{
+                mc.setAnnualInterest(annualInterest);
+                break;
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
 
-        MortgageCalculator mc = new MortgageCalculator(principal, annualInterest, years);
+        while(true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+
+            try{
+                mc.setYears(years);
+                break;
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
 
         System.out.println(mc.toString());
     }
